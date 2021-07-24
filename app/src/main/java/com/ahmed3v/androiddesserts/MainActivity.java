1b0869c3b2b04d09c9bbf2208c086ef7f54993ce
iAhmed3v <devahmdali@gmail.com>
2021-07-24 14:41:55 +0300

    Creating menu item

--- app/src/main/java/com/ahmed3v/androiddesserts/MainActivity.java
+++ app/src/main/java/com/ahmed3v/androiddesserts/MainActivity.java
@@ -1,9 +1,15 @@
 package com.ahmed3v.androiddesserts;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -12,13 +18,19 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "TAG";
     private ImageView donutImage, froyoImage, iceCreamImage;
     private FloatingActionButton fab;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
 
         donutImage = findViewById(R.id.donut_img);
         froyoImage = findViewById(R.id.froyo_img);
@@ -48,5 +60,39 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, OrderActivity.class);
             startActivity(intent);
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+
+            case R.id.action_contact:
+                Toast.makeText(this , R.string.action_contact_toast, Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_orders:
+                Toast.makeText(this , R.string.action_order_toast, Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_status:
+                Toast.makeText(this , R.string.action_status_toast, Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_favorites:
+                Toast.makeText(this , R.string.action_favorites_toast, Toast.LENGTH_SHORT).show();
+                return true;
+
+        }
+
+        return true;
     }
 }
